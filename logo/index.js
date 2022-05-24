9b458523cba460863910e622d54db744b1909a06
Shashwath Uchil <shashwath.uchil@niveussolutions.com>
2022-05-24 12:36:36 +0530

    image logo added

---
+++ logo/index.js
@@ -0,0 +1,11 @@
+import logo from '../../../logo.svg';
+
+const Card2 = () => {
+  return (
+    <div className="img">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Card2;
